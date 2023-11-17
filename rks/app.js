93adea3b2b6b62ba2c536b6f16914bf03ee8aa3e
TankEvans91 <151197749+TankEvans91@users.noreply.github.com>
2023-11-18 00:25:15 +0100

    Add files via upload

---
+++ rks/app.js
@@ -0,0 +1,5 @@
+const THUMBNAILS = document.querySelector(".thumbnail img");
+
+      THUMBNAILS.forEach((THUMBNAIL) => {
+      THUMBNAIL.addEventListener("click", () => console.log (THUMBNAIL));       
+            });
